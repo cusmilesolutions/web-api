ebeffd8040e6a28d951e30a3623ea701aaff07b2
unknown <frimpongi50@gmail.com>
2020-07-25 17:52:37 +0000

    made fixes to index.js

--- index.js
+++ index.js
@@ -7,33 +7,34 @@ const resolvers = require('./resolvers');
 
 require('dotenv').config();
 
-(async () => {
-  try {
-    mongoose
-      .connect(
-        // 'mongodb://localhost:27017/ecl-web',
-        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-jw97h.mongodb.net/${process.env.MONGODB_DB}`,
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      )
-      .then(() => console.log('Database connected'));
-    const app = express();
-    const auth = require('./middlewares/auth');
-    app.use(auth);
-    const server = new ApolloServer({
-      typeDefs,
-      resolvers,
-      playground: true,
-      context: ({ req, res }) => ({ req, res }),
-    });
-    server.applyMiddleware({ app });
-    const PORT = process.env.PORT || 5000;
+const app = express();
+const auth = require('./middlewares/auth');
+app.use(auth);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  context: ({ req, res }) => ({ req, res }),
+});
+server.applyMiddleware({ app });
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(
+    // 'mongodb://localhost:27017/ecl-web',
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-jw97h.mongodb.net/${process.env.MONGODB_DB}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('Database connected');
+  })
+  .then(() => {
     app.listen(PORT, () =>
       console.log(`http://localhost:${process.env.PORT}${server.graphqlPath}`)
     );
-  } catch (err) {
-    console.error(err);
-  }
-})();
+  })
+  .catch((err) => console.log(err));
